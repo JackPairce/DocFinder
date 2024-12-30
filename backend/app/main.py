94c61310152cba20f44405a6a310a7cb40d7222a
JackPairce <worldofspeeders@gmail.com>
2024-12-30 01:41:49 +0100

    Add CORS support to Flask app for local development; allow POST requests from frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,19 @@
 from flask import Flask
+from flask_cors import CORS
 from .routes import hello
 from .routes import suggestion
 
 app = Flask(__name__)
+
+CORS(
+    app,
+    resources={
+        r"/*": {
+            "origins": "http://localhost:3000",
+            "methods": ["POST"],
+        }
+    },
+)
 
 app.register_blueprint(hello.bp)
 app.register_blueprint(suggestion.bp)
